4551ee9f7a3b30de23770434b0b1849619345675
ubqai <liyixiaoqd@qq.com>
2017-03-05 10:59:04 +0800

    scan

--- application/wechat/views.py
+++ application/wechat/views.py
@@ -95,8 +95,9 @@ def server_authentication():
 
                 element_root.appendChild(element_content)
             elif text_event=="scancode_push" or text_event=="scancode_waitmsg":
-                text_sci=root.getElementsByTagName('ScanCodeInfo')[0].firstChild.data
-                text_sr=root.getElementsByTagName('ScanResult')[0].firstChild.data
+                input_element_scan_info=root.getElementsByTagName('ScanCodeInfo')[0]
+                text_sci=input_element_scan_info.getElementsByTagName('ScanCodeInfo')[0].firstChild.data
+                text_sr=input_element_scan_info.getElementsByTagName('ScanResult')[0].firstChild.data
                 element_content = ret_doc.createElement('Content') 
                 text_content = ret_doc.createTextNode("扫描["+text_sci+"]"+"成功["+text_sr+"],请等待处理")
                 element_content.appendChild(text_content)
