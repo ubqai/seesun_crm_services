c2cc8237bc641083349d3e21fdcb5a479bdc2bd0
caichang <chang.cai@quaie.com>
2017-03-17 11:03:33 +0800

    fix migration conflict

--- migrations/versions/cfd51dd8aaa0_add_available_number_to_material_.py
+++ migrations/versions/cfd51dd8aaa0_add_available_number_to_material_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'cfd51dd8aaa0'
-down_revision = '9fea66319b4a'
+down_revision = 'dc4da3e2992c'
 branch_labels = None
 depends_on = None
 
