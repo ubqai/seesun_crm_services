66c776ee70ecaef772fc0dfb65267d3a2dc27b31
ubqai <liyixiaoqd@qq.com>
2017-04-10 12:15:26 +0800

    form增加默认值

--- application/backstage_management/forms.py
+++ application/backstage_management/forms.py
@@ -18,15 +18,15 @@ class WechatUserLoginForm(AccountLoginForm):
 # BASE USER
 class AccountForm(BaseCsrfForm):
     email = StringField('邮箱', [validators.Email(message="请填写正确格式的email")])
-    name = StringField('姓名', [validators.Length(min=2, max=30, message="字段长度必须大等于2小等于30")])
-    nickname = StringField('昵称', [validators.Length(min=2, max=30, message="字段长度必须大等于2小等于30")])
+    name = StringField('姓名', default="", validators=[validators.Length(min=2, max=30, message="字段长度必须大等于2小等于30")])
+    nickname = StringField('昵称', default="", validators=[validators.Length(min=2, max=30, message="字段长度必须大等于2小等于30")])
     password = PasswordField('密码', validators=[
         validators.DataRequired(message="字段不可为空"),
         validators.Length(min=8, max=20, message="字段长度必须大等于8小等于20"),
         validators.EqualTo('password_confirm', message="两次输入密码不匹配")
     ])
     password_confirm = PasswordField('密码')
-    address = TextAreaField('地址', [validators.Length(min=5, max=300, message="字段长度必须大等于5小等于300")])
+    address = TextAreaField('地址', default="", validators=[validators.Length(min=5, max=300, message="字段长度必须大等于5小等于300")])
     # 电话匹配规则 11位手机 or 3-4区号(可选)+7-8位固话+1-6分机号(可选)
     phone = StringField('电话',
                         [validators.Regexp(r'(^\d{11})$|(^(\d{3,4}-)?\d{7,8}(-\d{1,5})?$)', message="请输入正确格式的电话")])
