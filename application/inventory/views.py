2b5e2b079811fba4499c141176f40a10d5bb4c43
fuyuan <lifuyuan33@gmail.com>
2017-04-14 15:39:16 +0800

    n

--- application/inventory/views.py
+++ application/inventory/views.py
@@ -14,8 +14,9 @@ inventory = Blueprint('inventory', __name__, template_folder='templates')
 @inventory.route('/', methods=['GET'])
 @cache.cached(timeout=500)
 def index():
-    skus = load_all_skus({'option_ids': []})
-    current_app.logger.info(skus)
+    skus = load_all_skus({'option_ids': [], 'page': request.args.get('page', 1),
+                          'page_size': request.args.get('page', 20)})
+
     return render_template('inventory/index.html', skus=skus)
 
 
